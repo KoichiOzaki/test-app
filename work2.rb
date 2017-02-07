af857b6fc640c5a309561857b59cdfe9fc188fa9
KoichiOzaki <goingmyway19851224@gmail.com>
2017-02-07 18:09:05 +0900

    fix

--- work2.rb
+++ work2.rb
@@ -8,5 +8,5 @@ puts "日を入力してください："
 day = gets.to_i
 
 date = Date.new(year,month,day)
-week = ["日","月","火","水","木","金","土"]
+week = ["月","火","水","木","金","土","日"]
 puts "#{year}年#{month}月#{day}日は#{week[date.wday]}曜日です"
